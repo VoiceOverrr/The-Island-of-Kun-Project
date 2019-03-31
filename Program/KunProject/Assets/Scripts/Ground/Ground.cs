27699d2c2efb9bc114e42e1d25e82fb3353ef60a
LudoArt <707779005@qq.com>
2019-03-31 18:30:34 +0800

    Week One

---
+++ Program/KunProject/Assets/Scripts/Ground/Ground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+    public float rorateSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+
+    void FixedUpdate()
+    {
+        float r = Input.GetAxis("Rotate");
+        transform.Rotate(new Vector3(0, 0, 1), r * Time.deltaTime * rorateSpeed);
+    }
+}
---
+++ Program/KunProject/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool isJumping;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
+	}
+
+    void FixedUpdate()
+    {
+        //角色移动
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //判断角色是否在地面上
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        //角色移动和站立的动画切换
+        if(moveInput == 0)
+        {
+            animator.SetBool("Run", false);
+        }
+        else
+        {
+            animator.SetBool("Run", true);
+        }
+
+        //角色转向
+        if(moveInput > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        
+        //角色跳跃
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("takeOf");
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if(isGrounded == true)
+        {
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            animator.SetBool("Jump", true);
+        }
+        if (Input.GetButton("Jump") && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
+	}
+}
